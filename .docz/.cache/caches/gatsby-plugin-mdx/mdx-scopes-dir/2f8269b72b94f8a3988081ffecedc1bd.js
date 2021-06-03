8ea5dc3f21a5aa9d5d7a2c4214e42afda038282a
Sergey <bigcheeseh@gmail.com>
2021-06-03 13:16:21 +0300

    remove docz

--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/2f8269b72b94f8a3988081ffecedc1bd.js
+++
@@ -1,9 +0,0 @@
-import { Playground, Props } from 'docz';
-import { Button } from "../../../../../docz/components/Button.tsx";
-import * as React from 'react';
-export default {
-  Playground,
-  Props,
-  Button,
-  React
-};
