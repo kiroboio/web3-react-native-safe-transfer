9221f8ac376a3c534db84f6ee227596f3b4353d0
Sergey <bigcheeseh@gmail.com>
2021-08-16 14:31:51 +0300

    update static docs

--- WebDocs/src/pages/index.js
+++ WebDocs/src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import {Redirect} from '@docusaurus/router';
-import HomepageFeatures from '../components/HomepageFeatures';
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -11,9 +10,6 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <main>
-        <HomepageFeatures />
-      </main>
       <Redirect to="/web3-lib/docs/intro" />;
     </Layout>
   )
